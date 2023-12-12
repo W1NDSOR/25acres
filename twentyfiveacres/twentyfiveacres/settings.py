3b92d86e3f9a557f5671427eccce69a526c41bee
Ankit Kumar <ankit21015@iiitd.ac.in>
2023-12-12 20:37:08 +0530

    Update settings.py
    smtp password update

--- twentyfiveacres/twentyfiveacres/settings.py
+++ twentyfiveacres/twentyfiveacres/settings.py
@@ -145,7 +145,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 465 
 EMAIL_HOST_USER = "acrestwentyfive@gmail.com"
-EMAIL_HOST_PASSWORD = "ahzcyvrqtcteazbs"
+EMAIL_HOST_PASSWORD = "qvfexymvltmyscps"
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True 
 
